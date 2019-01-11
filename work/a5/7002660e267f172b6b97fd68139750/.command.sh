c55da32c2d37d57d3127286c855ed196ece70d71
prakruthiburra <burraprakruthi@gmail.com>
2019-01-11 11:38:04 -0600

    Delete .command.sh

--- work/a5/7002660e267f172b6b97fd68139750/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-nextflow run /projects/bioinformatics/PrakruthiWork/Genomics_MGC_VariantCalling_Nextflow/src/nf_scripts/bqsr.nf -c /projects/bioinformatics/PrakruthiWork/nf_config/bqsr.config
